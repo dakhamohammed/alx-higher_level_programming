9a45f316ddedeedbf64e15529446359bab2fa609
dakhamohammed <isanagy@live.fr>
2023-09-05 14:33:42 -0700

    Python program that prints numbers from 0 to 99, Numbers should be printed in ascending order, with two digits.

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 for numbers in range(0, 100):
-    print("{:02}".format(numbers), end=", ")
-    if numbers == 99:
+    if numbers < 99:
+        print("{:02}".format(numbers), end=", ")
+    else:
         print("{}".format(numbers))
