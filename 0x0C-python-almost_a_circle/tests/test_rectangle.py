b772748173e33e1fd5fb27bf51eac0b837b1500a
dakhamohammed <isanagy@live.fr>
2023-10-16 17:14:48 -0700

    unittest Rectangle class.

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -792,10 +792,10 @@ class TestRectangle_to_dictionary(unittest.TestCase):
         self.assertDictEqual(correct, r.to_dictionary())
 
     def test_to_dictionary_no_object_changes(self):
-        r1 = Rectangle(10, 2, 1, 9, 5)
-        r2 = Rectangle(5, 9, 1, 2, 10)
-        r2.update(**r1.to_dictionary())
-        self.assertNotEqual(r1, r2)
+        rect1 = Rectangle(10, 2, 1, 9, 5)
+        rect2 = Rectangle(5, 9, 1, 2, 10)
+        rect2.update(**rect1.to_dictionary())
+        self.assertNotEqual(rect1, rect2)
 
     def test_to_dictionary_arg(self):
         r = Rectangle(10, 2, 4, 1, 2)
