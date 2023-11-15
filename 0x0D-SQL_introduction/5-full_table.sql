e9b8d4b6f757dca3b58d3bcb476b08a186104c1a
dakhamohammed <isanagy@live.fr>
2023-11-14 17:31:48 -0800

    SQL script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,4 @@
--- SQL script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- SQL script that prints the full description of the table first_table
+-- from the database hbtn_0c_0 in your MySQL server.
 DESCRIBE first_table
 
