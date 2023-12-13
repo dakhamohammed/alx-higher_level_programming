8e3e3abb0906f10d638a7496efb8a935de58e5d8
dakhamohammed <isanagy@live.fr>
2023-12-13 15:45:33 -0800

    function that converts a number from base 10 to another base passed as argument.

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  const convert = (number) => number.toString(base);
-  return convert;
+  return (number) => {
+    return number.toString(base);
+  };
 };
