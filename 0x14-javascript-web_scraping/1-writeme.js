9d2072d3965d7549f0b6cf3a4409cbff3e569155
dakhamohammed <isanagy@live.fr>
2024-02-16 14:52:19 -0500

    script that writes a string to a file.

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,6 +2,5 @@
 // script that writes a string to a file.
 const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], error => {
-  if (error) console.log(error);
+    if (error) console.log(error);
 });
-
