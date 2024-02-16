232a0503944cf884834b6023cc209ebf5882e3fa
dakhamohammed <isanagy@live.fr>
2024-02-16 14:44:58 -0500

    Script that reads and prints the content of a file.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-
 const fs_lib = require('fs');
-
 fs_lib.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+	console.log(error || content);
 });
+
